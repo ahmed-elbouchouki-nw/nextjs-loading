17eeb9e6b6c370a963da24fed7553d490e6c75c7
ahmed ELBOUCHOUKI <ahmed.el-bouchouki@nimbleways.com>
2024-04-23 10:54:29 +0100

    using nextjs loading

---
+++ src/app/pokemon/[name]/loading.tsx
@@ -0,0 +1,7 @@
+import type { FC } from "react";
+
+const LoadingPokemon: FC = () => {
+  return <div>Loading...</div>;
+};
+
+export default LoadingPokemon;
--- src/app/pokemon/[name]/page.tsx
+++ src/app/pokemon/[name]/page.tsx
@@ -1,21 +1,12 @@
-import { Suspense } from "react";
 import Pokemon from "./pokemon";
-
-
-function Loading(){
-  return (
-    <div>Loading...</div>
-  )
-}
-
 
 export default function Home({ params }: { params: { name: string } }) {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
       <h1>Pokémon !</h1>
-      <Suspense fallback={<Loading/>}>
-        <Pokemon pokemonName={params.name} />
-      </Suspense>
+      <Pokemon pokemonName={params.name} />
     </div>
   );
 }
